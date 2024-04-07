283255f762b0ab0b25081daa719a3322ef590e09
artemiusgreat <arteinvolo@gmail.com>
2024-04-06 20:34:14 -0400

    Gateways

--- Client/Components/BaseComponent.razor.cs
+++ Client/Components/BaseComponent.razor.cs
@@ -13,19 +13,15 @@ namespace Client.Components
     /// <summary>
     /// Updater
     /// </summary>
-    protected virtual ScheduleService Updater { get; set; } = new ScheduleService();
+    protected virtual ScheduleService Updater { get; set; } = InstanceService<ScheduleService>.Instance;
 
     /// <summary>
     /// Render
     /// </summary>
     protected virtual Task Render(Action action) => Updater.Send(() =>
     {
-      lock (protection)
-      {
-        action();
-        InvokeAsync(StateHasChanged);
-        Thread.Sleep(1);
-      }
+      action();
+      InvokeAsync(StateHasChanged);
     }).Task;
   }
 }
